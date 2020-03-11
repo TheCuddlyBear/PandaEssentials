de3f15cf5f714a04f773956a7e545439ea178b64
Bas van 't Hazeveld <bas@hazeveld.org>
2020-03-11 17:50:12 +0100

    Finished the teleport command in teleport.java

--- src/main/java/me/pandaplugins/pandaessentials/commands/teleport.java
+++ src/main/java/me/pandaplugins/pandaessentials/commands/teleport.java
@@ -1,7 +1,9 @@
 package me.pandaplugins.pandaessentials.commands;
 
 import me.pandaplugins.pandaessentials.PandaEssentials;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -19,10 +21,29 @@ public class teleport implements CommandExecutor {
             Player player = (Player) sender;
             if(args.length == 1){
                 if(player.hasPermission("pandaessentials.teleport.others")){
-
+                    Player target = Bukkit.getPlayerExact(args[0]);
+                    if(target != null){
+                        Location targetLocation = target.getLocation();
+                        player.teleport(targetLocation);
+                    }else{
+                        player.sendMessage(ChatColor.RED + "That player does not exist!");
+                    }
+                }else{
+                    player.sendMessage(ChatColor.RED + "Invalid permissions!");
                 }
             }else if(args.length == 2) {
-                if(player.hasPermission("pandaessentials.teleport.others"));
+                if(player.hasPermission("pandaessentials.teleport.others")){
+                    Player player1 = Bukkit.getPlayerExact(args[0]);
+                    Player player2 = Bukkit.getPlayerExact(args[1]);
+                    if(player1 != null && player2 != null){
+                        Location player2Location = player.getLocation();
+                        player1.teleport(player2Location);
+                    }else{
+                        player.sendMessage(ChatColor.RED + "That player does not exist!");
+                    }
+                }else{
+                    player.sendMessage(ChatColor.RED + "Invalid permissions!");
+                }
             } else{
                 player.sendMessage(ChatColor.RED + "Invalid arguments!");
                 player.sendMessage(ChatColor.RED + "/teleport <playername> [otherplayer]");
