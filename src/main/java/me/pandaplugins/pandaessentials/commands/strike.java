5a218aeb0cd100259bf39b2e603003abba3dc3c8
Bas van 't Hazeveld <bas@hazeveld.org>
2020-03-13 11:59:47 +0100

    Started work on strike command

---
+++ src/main/java/me/pandaplugins/pandaessentials/commands/strike.java
@@ -0,0 +1,31 @@
+package me.pandaplugins.pandaessentials.commands;
+
+import me.pandaplugins.pandaessentials.PandaEssentials;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+public class strike implements CommandExecutor {
+
+    Plugin plugin = PandaEssentials.getPlugin(PandaEssentials.class);
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(sender instanceof Player){
+            Player player = (Player) sender;
+            Block target = player.getTargetBlockExact(30);
+            Location loc = target.getLocation();
+
+
+        }else{
+            plugin.getLogger().severe("This command has be run as a player");
+        }
+
+        return true;
+    }
+}
