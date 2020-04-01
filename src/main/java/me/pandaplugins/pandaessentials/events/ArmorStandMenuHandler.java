1e7ab0ceeff813900a59935843931ae66f7ce573
TheCuddlyBear <bas@hazeveld.org>
2020-04-01 13:14:48 +0200

    Removed unneccesary import

--- src/main/java/me/pandaplugins/pandaessentials/events/ArmorStandMenuHandler.java
+++ src/main/java/me/pandaplugins/pandaessentials/events/ArmorStandMenuHandler.java
@@ -7,7 +7,6 @@ import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
